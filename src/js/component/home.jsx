03ab3cda0783446301ad839a10946c8e94f599aa
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-07-04 10:24:22 +0000

    Estructura Body

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,15 +8,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div id="myApp" className="text-center">
+			
 			<div className="navBar"></div>
-			{/* 1. navbar */}
+			
 			<div className="container">
-			{/* 2. body: dentro del div classs container
-				2.1. Jumbotron
-				2.2. Cards */}
+				<div className="jumbotrom"></div>
+				<div className="cards"></div>
 			</div>
+			
 			<div className="footer"></div>
-			{/* 3.footer */}
+		
 		</div>
 	);
 };
