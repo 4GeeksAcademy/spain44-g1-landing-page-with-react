1e5e93c882bac7c229115c2e6deeaa0eaf424e39
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-07-05 16:44:57 +0000

    feat: add Sidebar.jsx

--- src/js/component/SIdeBar.jsx
+++ src/js/component/SIdeBar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Container } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 export const SideBar = () => {
 
     return (
-        <h1>Side Bar</h1>
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container fluid>
+          <Navbar.Brand>
+            Una Marca
+          </Navbar.Brand>
+          <Navbar.Text className={'d-none d-sm-inline'}>
+            <strong>{"hola"}</strong><br/>
+            <small>{"Hola"}</small>
+          </Navbar.Text>
+          <Navbar.Text className={'d-inline d-sm-none'}>
+            text
+          </Navbar.Text>
+        </Container>
+        </Navbar>
     )
 }
